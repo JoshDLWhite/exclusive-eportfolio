a531dd5a57ae601041d3f3c9043e8903bbf172ae
Josh Lillywhite <JoshDLWhite@gmail.com>
2026-08-26 09:26:19 -0700

    adding animation to modal halves and responsive buttons to call and dismiss them. Having issues with the fading in of the left side modal.

--- index.js
+++ index.js
@@ -21,3 +21,14 @@ function contact() {
     alert("The email service is temporarily unavailable. Please contact me directly at JoshDLWhite@gmail.com");
   })
 }
+
+let isModalOpen = false;
+function toggleModal() {
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+    
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
+}
